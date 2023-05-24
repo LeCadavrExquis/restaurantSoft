9c6622494a7aec1da51ac83f4fcf261b2c5dcfd7
dawid <dawid.potwora.stud@pw.edu.pl>
2023-05-24 20:02:12 +0200

    proba

--- src/ui/TablesView.java
+++ src/ui/TablesView.java
@@ -1,9 +1,21 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TablesView extends JPanel {
     public TablesView() {
-        this.add(new JLabel("belko"));
+        GridLayout layout = new GridLayout(4,4);
+        this.setLayout(layout);
+
+        for (int i = 0; i < 6; i++) {
+            String btnName = "" + i;
+            JButton button = new JButton("");
+            button.setBackground(Color.RED);
+            this.add(button);
+            JButton button2 = new JButton(btnName);
+            button2.setBackground(Color.GREEN);
+            this.add(button2);
+        }
     }
 }
