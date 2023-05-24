3df786dabefb300ba90f1b6d7dca8b0d0239ba99
artur <art.wencel@gmail.com>
2023-05-24 18:43:08 +0200

    User class

---
+++ src/User.java
@@ -0,0 +1,11 @@
+class User {
+    int id;
+    int password;
+    boolean isAdmin;
+
+    public User(int id, int password, boolean isAdmin) {
+        this.id = id;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+}
