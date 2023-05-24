a57c97347eab813c4b25db6c67d69e0e90b5a713
artur <art.wencel@gmail.com>
2023-05-24 18:45:49 +0200

    User class getters and setters

--- src/User.java
+++ src/User.java
@@ -1,11 +1,35 @@
 class User {
-    int id;
-    int password;
-    boolean isAdmin;
+    private int id;
+    private int password;
+    private boolean isAdmin;
 
     public User(int id, int password, boolean isAdmin) {
         this.id = id;
         this.password = password;
         this.isAdmin = isAdmin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
     }
 }
