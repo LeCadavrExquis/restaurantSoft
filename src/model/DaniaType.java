0968fc7920922b38a72f51434dbdcd27a2da11f5
asierpi1 <01158535@pw.edu.pl>
2023-05-31 19:52:26 +0200

    plates modyfikacja

--- src/model/DaniaType.java
+++ src/model/DaniaType.java
@@ -4,7 +4,10 @@ public enum DaniaType {
     PIZZA("pizza", 32.0),
     DRINK("napój", 5.5),
     PASTA("makaron", 27),
-    PIEROGI("pierogi", 25.0);
+    PIEROGI("pierogi", 25.0),
+
+    WODA("woda",5.0);
+
     public String name;
     public double price;
 
