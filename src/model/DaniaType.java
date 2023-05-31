fab7f79b8d0d36f6d059a23af1db580112aeba18
puszekhonor <puszekhonor@gmail.com>
2023-05-31 19:52:40 +0200

    Loginview

--- src/model/DaniaType.java
+++ src/model/DaniaType.java
@@ -4,7 +4,9 @@ public enum DaniaType {
     PIZZA("pizza", 32.0),
     DRINK("napój", 5.5),
     PASTA("makaron", 27),
-    PIEROGI("pierogi", 25.0);
+    PIEROGI("pierogi", 25.0),
+    NALESNIK ("nalesnik", 17.5);
+
     public String name;
     public double price;
 
